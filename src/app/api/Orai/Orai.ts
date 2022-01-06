0b0b84da05052ee9cd25ee7f954f14331081cf07
AurimasUrbonas <aurbonas13@gmail.com>
2022-01-06 19:42:25 +0200

    fixed weather

--- src/app/api/Orai/Orai.ts
+++ src/app/api/Orai/Orai.ts
@@ -4,7 +4,7 @@ const weatherAPIKey = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const Orai = async () => {
   const weatherResponse = await axios({
-    url: `http://api.weatherapi.com/v1/forecast.json?key=${weatherAPIKey}&q=Vilnius&days=1&aqi=no&alerts=no`,
+    url: `https://api.weatherapi.com/v1/forecast.json?key=${weatherAPIKey}&q=Vilnius&days=1&aqi=no&alerts=no`,
     method: "GET",
   });
 
